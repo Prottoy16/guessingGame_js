fa4ce445a2e4b1c191f3c21278b596921bfef16a
Prottoy16 <prottoybiswas16@gmail.com>
2023-11-24 22:09:25 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,24 @@
+
+
+var numOfwon=0;
+var numOflost=0;
+
+ for(var i=0;i<5;i++){
+
+    var guessNumber=parseInt(prompt("Enter a number between 1 to 5 : "));
+
+    var randomNumber=Math.floor(Math.random()*5)+1;
+
+    if(guessNumber==randomNumber){
+        console.log("You have won");
+        numOfwon++;
+    }
+    else {
+        console.log("You have lost. Random number was : "+randomNumber);
+        numOflost++;
+    }
+
+ }
+
+ document.write("Total number of won is : "+numOfwon+"<br> ");
+ document.write("Total number of lost is : "+numOflost);
